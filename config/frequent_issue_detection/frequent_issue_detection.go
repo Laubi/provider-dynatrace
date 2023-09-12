6ae78046653dfe81088a2c235df2d549ad352bf3
David Laubreiter <david.laubreiter@dynatrace.com>
2023-09-12 09:55:05 +0200

    Step 7: Add frequent issue detection configuration provider
    (cherry picked from commit 9879fcb287cc60c45d2454f42be84c0435bcde46)

---
+++ config/frequent_issue_detection/frequent_issue_detection.go
@@ -0,0 +1,10 @@
+package frequent_issue_detection
+
+import "github.com/upbound/upjet/pkg/config"
+
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("frequent_issue_detection", func(r *config.Resource) {
+		r.ShortGroup = "frequent_issue_detection"
+	})
+}
